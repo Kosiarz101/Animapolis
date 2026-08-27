d4c997e1db0e82336849c5b3af1bb554878c8949
Kosiarz101 <pol1100@o2.pl>
2026-08-27 11:17:17 +0200

    add Eureka discovery server

---
+++ eureka/src/test/java/org/animapolis/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.animapolis.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
